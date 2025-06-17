ffaeb7bd54b10a3ffb7bc0dcc27824a2a000943d
He23333000 <he23333000@gmail.com>
2025-06-17 22:54:44 +0800

    first commit

--- src/store.js
+++ src/store.js
@@ -1,8 +1,13 @@
+// store.js
+
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CartSlice';
- const store = configureStore({
-    reducer: {
-        cart: cartReducer,
-    },
+import cartReducer from './CartSlice'; // Make sure the path is correct
+
+// Create the Redux store with cart slice
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
 });
-export default store
+
+export default store;
